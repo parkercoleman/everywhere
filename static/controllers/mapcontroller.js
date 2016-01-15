0ef213dc841654fe201a213267df4298f00fd833
Parker Coleman <ADINSX.sa@gmail.com>
2016-01-14 20:36:10 -0800

    routes now showing up on map

--- static/controllers/mapcontroller.js
+++ static/controllers/mapcontroller.js
@@ -1,9 +1,10 @@
 (function() {
     var app = angular.module('otbp', ['ui.bootstrap']);
 
-    app.controller('MapController', ['$http',  function($http){
+    app.controller('MapController', ['$scope', '$http',  function($scope, $http){
         this.startLoc = undefined;
         this.endLoc = undefined;
+        this.currentRoute = undefined;
 
         this.getCitiesFromPartial = function(cityName) {
             return $http.get('/graph/places/' + encodeURIComponent(cityName))
@@ -12,10 +13,25 @@
             });
         };
 
-        this.calculateRoute = function($http){
-            $http.get('/calc_route/from/' + this.startLoc.gid + '/to/' + this.endLoc.gid)
+        this.calculateRoute = function(){
+            $http.get('/graph/calc_route/from/' + this.startLoc.gid + '/to/' + this.endLoc.gid)
                 .then(function(response){
+                    this.currentRoute = response.data.route_id;
 
+                    map.removeLayer(route);
+
+                    var route = new ol.layer.Image({
+                            source: new ol.source.ImageWMS({
+                            url: 'http://localhost:8080/geoserver/wms',
+                            params: {
+                                'LAYERS': 'otbp:user_routes',
+                                'CQL_FILTER':"route_id='" + this.currentRoute + "'"
+                            },
+                            serverType: 'geoserver'
+                        })
+                    });
+
+                    map.addLayer(route);
                 });
         };
     }]);
