aa6ce30ad2d182d0276e08ae3121ebaf992c5477
Parker Coleman <ADINSX.sa@gmail.com>
2016-01-15 20:27:41 -0800

    rudamentary instructions

--- static/controllers/mapcontroller.js
+++ static/controllers/mapcontroller.js
@@ -14,24 +14,28 @@
         };
 
         this.calculateRoute = function(){
+            var ctrl = this;
             $http.get('/graph/calc_route/from/' + this.startLoc.gid + '/to/' + this.endLoc.gid)
                 .then(function(response){
-                    this.currentRoute = response.data.route_id;
+                    ctrl.currentRoute = response.data;
 
-                    map.removeLayer(route);
-
-                    var route = new ol.layer.Image({
-                            source: new ol.source.ImageWMS({
-                            url: 'http://localhost:8080/geoserver/wms',
-                            params: {
-                                'LAYERS': 'otbp:user_routes',
-                                'CQL_FILTER':"route_id='" + this.currentRoute + "'"
-                            },
-                            serverType: 'geoserver'
-                        })
+                    var currentRouteLayer = L.tileLayer.wms("http://localhost:8080/geoserver/wms", {
+                        layers: 'otbp:user_routes',
+                        format: 'image/png',
+                        'CQL_FILTER':"route_id='" + ctrl.currentRoute.route_id + "'",
+                        attribution: "Off the Beatnik Path",
+                        transparent: true
                     });
 
-                    map.addLayer(route);
+                    // Remove the old user_routes layer
+                    map.eachLayer(function(layer) {
+                        if(layer.wmsParams !== undefined && layer.wmsParams.layers === 'otbp:user_routes'){
+                            map.removeLayer(layer);
+                        }
+                    });
+
+                    // add the new one
+                    map.addLayer(currentRouteLayer);
                 });
         };
     }]);
